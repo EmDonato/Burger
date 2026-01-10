d6f449ffda0307149834907e583b826a92f2f647
EmDonato <mcf.donato@gmail.com>
2026-01-10 16:33:58 +0100

    10/01/2026

--- src/stm32_nucleo_f303re_driver/include/stm32_nucleo_f303re_driver/parsing_types.hpp
+++ src/stm32_nucleo_f303re_driver/include/stm32_nucleo_f303re_driver/parsing_types.hpp
@@ -1,8 +1,13 @@
 #pragma once
 #include <cstdint>
 
-enum class msg_ID : uint8_t
-{
+// =====================
+// PROTOCOL CONSTANTS
+// =====================
+static constexpr uint8_t HDR1 = 0xAA;
+static constexpr uint8_t HDR2 = 0x55;
+
+enum MsgID : uint8_t {
     IMU_ID     = 0x01,
     ENC_ID     = 0x02,
     CMD_VEL_ID = 0x03,
@@ -11,8 +16,7 @@ enum class msg_ID : uint8_t
     STRING_ID  = 0x06
 };
 
-enum class parse_state : uint8_t
-{
+enum class ParseState {
     WAIT_HEADER1,
     WAIT_HEADER2,
     WAIT_TYPE,
@@ -21,26 +25,3 @@ enum class parse_state : uint8_t
     WAIT_CHECKSUM
 };
 
-struct msg_info
-{
-    // FSM
-    parse_state state = parse_state::WAIT_HEADER1;
-    msg_ID type{};
-
-    // protocol
-    uint8_t len = 0;                 
-    uint32_t tmp_numb = 0;            
-
-    // counters 
-    uint32_t numb_IMU = 0;
-    uint32_t numb_ENC = 0;
-    uint32_t numb_HB  = 0;
-
-    // payload
-    uint8_t payload[128]{};
-    uint8_t payload_idx = 0;
-
-    // checksum
-    uint8_t checksum = 0;
-    bool valid = false;
-};
